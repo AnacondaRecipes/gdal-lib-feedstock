c9c4e351dd262c1348b4cce5c1805f3adf21bf0f
Ilan Schnell <ilanschnell@gmail.com>
2022-02-10 15:22:02 -0600

    fix conda ClobberWarning

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -33,3 +33,6 @@ if errorlevel 1 exit 1
 
 copy %RECIPE_DIR%\scripts\deactivate.sh %DEACTIVATE_DIR%\gdal-deactivate.sh
 if errorlevel 1 exit 1
+
+del %LIBRARY_PREFIX%\copying
+del %LIBRARY_PREFIX%\release.txt
